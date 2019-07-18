0c3e69b231df519a6638163f6d656bcfcd7c67ea
student <student@my.sliit.lk>
2019-07-18 13:06:45 +0530

    added textView hello student

--- app/src/main/java/com/example/student/gitcloneproj/MainActivity.java
+++ app/src/main/java/com/example/student/gitcloneproj/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.student.gitcloneproj;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textView1 = findViewById(R.id.textView1);
+        textView1.setText("Hello student");
         System.out.println("Hello student");
     }
 }
